80396d935011bd1444a883c84746c6cb24b30fa5
Boss_Man <54071671+Boss-Man-Dev@users.noreply.github.com>
2022-09-27 21:39:23 -0500

    Updated Weapons
    Created weapon info lists in cfg
    created category system for weapon organization
    created sub menus for organization/usability
    
    Added functionality to weapon/component spawning.
    added weapons/components into the corresponding menus
    added enabled checks to weapons/components
    
    general clean up of code

--- vrp/modules/weapon.lua
+++ vrp/modules/weapon.lua
@@ -7,6 +7,29 @@ local lang = vRP.lang
 
 local Weapon = class("Weapon", vRP.Extension)
 
+local function menu_weapons_type_options_components(self)
+	vRP.EXT.GUI:registerMenuBuilder("weapons.type.options.components", function(menu)
+	  local user = menu.user
+  
+	  menu.title = menu.data.name
+	  menu.css.header_color = "rgba(200,0,0,0.75)"
+
+	  for k,v in pairs(self.weapons) do
+		if k == menu.data.weapon then
+			local n,d,g,c = table.unpack(v)
+			for k,v in pairs(c) do
+				local h,n,d,e = v.HashKey,v.Name,v.Description,v.Enabled
+				if e then
+					menu:addOption(n, function(menu)
+						vRP.EXT.Weapon.remote._giveComponent(menu.user.source, user.source, menu.data.weapon, h)
+					end, d)
+				end
+			end
+		  end
+	  end
+	end)
+  end
+
 local function menu_weapons_type_options(self)
   vRP.EXT.GUI:registerMenuBuilder("weapons.type.options", function(menu)
 	local user = menu.user
@@ -16,11 +39,10 @@ local function menu_weapons_type_options(self)
 	
 	menu:addOption("Give Weapon", function(menu)
 		self.remote._giveWeapon(menu.user.source, user.source, menu.data.weapon)
-		vRP.EXT.Base.remote._notify(menu.user.source,"You recieved a "..menu.data.weapon.."")
+		vRP.EXT.Base.remote._notify(menu.user.source,"You recieved a "..menu.data.name.."")
 	end)
-	menu:addOption("Give Component", function(menu)
-		local component = user:prompt("give weapon component full name", "")
-		vRP.EXT.Weapon.remote._giveComponent(menu.user.source, user.source, menu.data.weapon, string.upper(""..component))
+	menu:addOption("Components", function(menu)
+		menu.user:openMenu("weapons.type.options.components", {name = menu.data.name, weapon =  menu.data.weapon})
 	end)
 	menu:addOption("Give Ammo", function(menu)
 		local ammo = user:prompt("give weapon ammo amount", "")
@@ -34,15 +56,16 @@ local function menu_weapons_type(self)
   vRP.EXT.GUI:registerMenuBuilder("weapons.type", function(menu)
 	local user = menu.user
 
-    menu.title = menu.data.gtype
+    menu.title = menu.data.title
     menu.css.header_color = "rgba(200,0,0,0.75)"
 	
 	for weapon, v in pairs(self.weapons) do
-		local gtype, name, hash = table.unpack(v)
-		if menu.title == gtype then
+		local name,Description,Group,Components = table.unpack(v)
+
+		if menu.data.gtype == Group then
 			menu:addOption(name, function(menu)
 				menu.user:openMenu("weapons.type.options", {name = name, weapon = weapon})
-			end)
+			end, Description)
 		end
 	end
   end)
@@ -56,11 +79,14 @@ local function menu_weapons(self)
     menu.title = "Weapons"
     menu.css.header_color = "rgba(200,0,0,0.75)"
 	
-	for gtype, v in pairs(self.cfg.weapon_types) do
-		menu:addOption(gtype, function(menu)
-			menu.user:openMenu("weapons.type", {gtype = gtype})
+	for k,v in pairs(self.gtypes) do
+		local title = table.unpack(v)
+		
+		menu:addOption(title, function(menu)
+			menu.user:openMenu("weapons.type", {gtype = k, title = title})
 		end)
-    end
+	end
+
   end)
 end
 
@@ -69,32 +95,30 @@ function Weapon:__construct()
 
   self.cfg = module("vrp", "cfg/weapon")
   
-  self.weapons = {} -- map of all weapons
-  self.test = {} -- map of all weapons
+  self.weapons = {} -- map of all enabled weapons
+  self.gtypes = {} -- map of all enabled gtypes
 
   -- register all enabled weapons
-  for wepon,v in pairs(self.cfg.weapons) do
-	--self:log("\r\27[10;92m[K: "..wepon.."] \r\27[0m")
-	for k,v in pairs(v) do
-		--print(tostring(k))
-		--print("V: "..tostring(v))
+  for k,v in pairs(self.cfg.weapons) do
+	local h,n,d,g,e,c = v.HashKey,v.Name,v.Description,v.Group,v.Enabled,v.Components
+	if e then
+		self.weapons[h] = {n,d,g,c}
 	end
   end
   
-  --[[
-  for gtype, v in pairs(self.cfg.weapon_types) do
-	for weapon, v in pairs(v) do
-		if v[3] then	-- checks if weapon can be used
-			self.weapons[weapon] = {v[1], v[2]}
-		end
+  -- register all enabled gtypes
+  for k, v in pairs(self.cfg.types) do
+	local gcfg = v._config
+	if gcfg.enabled then
+		self.gtypes[k] = {gcfg.title}
 	end
   end
-  ]]--
-  
+
   --menu
   menu_weapons(self)
   menu_weapons_type(self)
   menu_weapons_type_options(self)
+  menu_weapons_type_options_components(self)
    
   -- list for all weapons that are useable
   vRP.EXT.GUI:registerMenuBuilder("admin", function(menu)
