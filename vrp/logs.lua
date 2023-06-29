8c0e2b3c3126f4dd5a4b708c352589e8ef1dec5e
Rafra <96952541+rafra1@users.noreply.github.com>
2023-06-29 09:54:00 +0300

    Discord Log System

---
+++ vrp/logs.lua
@@ -0,0 +1,33 @@
+local cfg = {}
+
+cfg.webhooks = {
+   player_join = "",
+   player_leave = "",
+   player_death = ""
+}
+
+cfg.sufix = "[vRP3]"
+
+cfg.server_icon = "https://github.com/vRP-framework/vRP/raw/master/misc/logo_alpha.png"
+cfg.avatar_url = "https://github.com/rafra1/vRP3-Dev/blob/main/misc/logo.png?raw=true"
+
+cfg.embeds = {
+   author = {
+      url = "https://github.com/vRP-framework/vRP3-Dev", -- default author url
+   },
+   colors = {
+      default = 0,
+      blue = 25087,
+      green = 89886,
+      white = 16777215,
+      black = 0,
+      orange = 16743168,
+      lightgreen = 65309,
+      yellow = 15335168,
+      turqois = 62207,
+      pink = 16711900,
+      red = 16711680,
+   },
+}
+
+return cfg
