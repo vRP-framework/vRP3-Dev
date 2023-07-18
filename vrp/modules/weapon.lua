fde7483c76b9eb14b64d5c1275589e16aad56f33
Boss_Man <54071671+Boss-Man-Dev@users.noreply.github.com>
2023-07-18 12:53:05 -0500

    Updated Weapons/Player-State & menu
    [Weapon State]
    
        Improved overall functionality
        Fixed issue with weapon and components not saving
        Implemented a check to prevent duplicate entries
    
    [Player State]
    
        Updated function calls for weapons
        Passed user information to weapon functions
    
    [Menus.css]
    
        Commented out the menu background image (Personal preference)

--- vrp/modules/weapon.lua
+++ vrp/modules/weapon.lua
@@ -7,87 +7,93 @@ local lang = vRP.lang
 
 local Weapon = class("Weapon", vRP.Extension)
 
-local function menu_weapons_type_options_components(self)
-	vRP.EXT.GUI:registerMenuBuilder("weapons.type.options.components", function(menu)
-	  local user = menu.user
-  
-	  menu.title = menu.data.name
-	  menu.css.header_color = "rgba(200,0,0,0.75)"
+local function componentsMenu(self)
+    vRP.EXT.GUI:registerMenuBuilder("weapons.type.options.components", function(menu)
+        local user = menu.user
 
-	  for k,v in pairs(self.weapons) do
-		if k == menu.data.weapon then
-			local n,d,g,c = table.unpack(v)
-			for k,v in pairs(c) do
-				local h,n,d,e = v.HashKey,v.Name,v.Description,v.Enabled
-				if e then
-					menu:addOption(n, function(menu)
-						vRP.EXT.Weapon.remote._giveComponent(menu.user.source, user.source, menu.data.weapon, h)
-					end, d)
-				end
-			end
-		  end
-	  end
-	end)
-  end
+        menu.title = menu.data.name
+        menu.css.header_color = "rgba(200,0,0,0.75)"
 
-local function menu_weapons_type_options(self)
-  vRP.EXT.GUI:registerMenuBuilder("weapons.type.options", function(menu)
-	local user = menu.user
+        local weaponData = self.weapons[menu.data.weapon]
+        if weaponData then
+            local _, _, _, components = table.unpack(weaponData)
+            for _, v in pairs(components) do
+                local hashKey, name, description, enabled = v.HashKey,v.Name,v.Description,v.Enabled
+                if enabled then
+                    local function giveComponent()
+                        vRP.EXT.Weapon.remote._giveComponent(menu.user.source, user.source, menu.data.weapon, hashKey)
+                    end
 
-    menu.title = menu.data.name
-    menu.css.header_color = "rgba(200,0,0,0.75)"
-	
-	menu:addOption("Give Weapon", function(menu)
-		self.remote._giveWeapon(menu.user.source, user.source, menu.data.weapon)
-		vRP.EXT.Base.remote._notify(menu.user.source,"You recieved a "..menu.data.name.."")
-	end)
-	menu:addOption("Components", function(menu)
-		menu.user:openMenu("weapons.type.options.components", {name = menu.data.name, weapon =  menu.data.weapon})
-	end)
-	menu:addOption("Give Ammo", function(menu)
-		local ammo = user:prompt("give weapon ammo amount", "")
-		vRP.EXT.Weapon.remote._giveAmmo(menu.user.source, user.source, menu.data.weapon, tonumber(ammo))
-		vRP.EXT.Base.remote._notify(menu.user.source,"You recieved "..ammo.." ammo")
-	end)
-  end)
+                    menu:addOption(name, giveComponent, description)
+                end
+            end
+        end
+    end)
 end
 
-local function menu_weapons_type(self)
-  vRP.EXT.GUI:registerMenuBuilder("weapons.type", function(menu)
-	local user = menu.user
+local function optionsMenu(self)
+    vRP.EXT.GUI:registerMenuBuilder("weapons.type.options", function(menu)
+        local user = menu.user
 
-    menu.title = menu.data.title
-    menu.css.header_color = "rgba(200,0,0,0.75)"
-	
-	for weapon, v in pairs(self.weapons) do
-		local name,Description,Group,Components = table.unpack(v)
+        menu.title = menu.data.name
+        menu.css.header_color = "rgba(200,0,0,0.75)"
 
-		if menu.data.gtype == Group then
-			menu:addOption(name, function(menu)
-				menu.user:openMenu("weapons.type.options", {name = name, weapon = weapon})
-			end, Description)
-		end
-	end
-  end)
+        local function giveWeapon(menu)
+            self.remote._giveWeapon(menu.user.source, user.source, menu.data.weapon)
+            vRP.EXT.Base.remote._notify(menu.user.source, "You received a " .. menu.data.name)
+        end
+
+        local function openComponentsMenu(menu)
+            menu.user:openMenu("weapons.type.options.components", { name = menu.data.name, weapon = menu.data.weapon })
+        end
+
+        local function giveAmmo(menu)
+            local ammo = user:prompt("give weapon ammo amount", "")
+            vRP.EXT.Weapon.remote._giveAmmo(menu.user.source, user.source, menu.data.weapon, tonumber(ammo))
+            vRP.EXT.Base.remote._notify(menu.user.source, "You received " .. ammo .. " ammo")
+        end
+
+        menu:addOption("Give Weapon", giveWeapon)
+        menu:addOption("Components", openComponentsMenu)
+        menu:addOption("Give Ammo", giveAmmo)
+    end)
+end
+
+local function weaponTypeMenu(self)
+    vRP.EXT.GUI:registerMenuBuilder("weapons.type", function(menu)
+        local user = menu.user
+
+        menu.title = menu.data.title
+        menu.css.header_color = "rgba(200,0,0,0.75)"
+
+        for weapon, data in pairs(self.weapons) do
+            local name, description, group, _ = table.unpack(data)
+
+            if menu.data.gtype == group then
+                local function openOptionsMenu()
+                    menu.user:openMenu("weapons.type.options", { name = name, weapon = weapon })
+                end
+
+                menu:addOption(name, openOptionsMenu, description)
+            end
+        end
+    end)
 end
 
 -- menu: admin
-local function menu_weapons(self)
-  vRP.EXT.GUI:registerMenuBuilder("weapons", function(menu)
-	local user = menu.user
+local function weaponsMenu(self)
+    vRP.EXT.GUI:registerMenuBuilder("weapons", function(menu)
+        local user = menu.user
 
-    menu.title = "Weapons"
-    menu.css.header_color = "rgba(200,0,0,0.75)"
-	
-	for k,v in pairs(self.gtypes) do
-		local title = table.unpack(v)
-		
-		menu:addOption(title, function(menu)
-			menu.user:openMenu("weapons.type", {gtype = k, title = title})
-		end)
-	end
+        menu.title = "Weapons"
+        menu.css.header_color = "rgba(200,0,0,0.75)"
 
-  end)
+        for gtype, title in pairs(self.gtypes) do
+            menu:addOption(title, function(menu)
+                menu.user:openMenu("weapons.type", { gtype = gtype, title = title })
+            end)
+        end
+    end)
 end
 
 function Weapon:__construct()
@@ -114,11 +120,11 @@ function Weapon:__construct()
 	end
   end
 
-  --menu
-  menu_weapons(self)
-  menu_weapons_type(self)
-  menu_weapons_type_options(self)
-  menu_weapons_type_options_components(self)
+  -- menu
+    weaponsMenu(self)
+    weaponTypeMenu(self)
+    optionsMenu(self)
+    componentsMenu(self)
    
   -- list for all weapons that are useable
   vRP.EXT.GUI:registerMenuBuilder("admin", function(menu)
