b240815d68e29f97349406005571b22f1421364c
Ianis Catalin <73122512+IanisCatalin15@users.noreply.github.com>
2025-05-01 20:15:28 +0300

    Update hours.lua

--- vrp/modules/hours.lua
+++ vrp/modules/hours.lua
@@ -3,52 +3,61 @@
 
 if not vRP.modules.hours then return end
 
-local hours = class("hours", vRP.Extension)
+local Hours = class("hours", vRP.Extension)
 
-hours.event = {}
-hours.tunnel = {}
+Hours.event = {}
+Hours.tunnel = {}
 
-function hours:__construct()
+function Hours:__construct()
     vRP.Extension.__construct(self)
-    RegisterCommand("timer", function(source) 
+    RegisterCommand("timer", function(source)
+        if source == 0 then return end
         vRP:triggerEvent("getHours", source)
     end, false)
 end
 
-function hours.event:getHours(source)
+function Hours.event:getHours(source)
     local user = vRP.users_by_source[source]
-    local hours = user.cdata.hours
-    local minutes = hours - math.floor(hours)
+    if not user or not user.cdata or not user.cdata.hours then return end
 
-    vRP.EXT.Base.remote._notify(user.source, "You have ~b~" .. math.floor(hours) .. " ~w~ hours and ~b~" .. math.floor(minutes * 100 * 0.60) .. " ~w~ minutes played!")
+    local hours = user.cdata.hours
+    local fullHours = math.floor(hours)
+    local minutes = math.floor((hours % 1) * 60)
+
+    local message = string.format("You have ~b~%d~w~ hours and ~b~%d~w~ minutes played!", fullHours, minutes)
+    vRP.EXT.Base.remote._notify(user.source, message)
 end
 
-function hours.event:updateHours(hoursToAdd)
+function Hours.event:updateHours(hoursToAdd)
+    if type(hoursToAdd) ~= "number" or hoursToAdd <= 0 then return end
+
     for _, user in pairs(vRP.users) do
         if user.cdata then
             local currentHours = user.cdata.hours or 0
             local newHour = currentHours + hoursToAdd
             user.cdata.hours = newHour
             user:save()
-            vRP.EXT.Base.remote._notify(user.source, "You now have ~b~" .. math.floor(newHour) .. " ~w~ hours and ~b~" .. math.floor((newHour - math.floor(newHour)) * 100 * 0.60) .. " ~w~ minutes played.")
+
+            local fullHours = math.floor(newHour)
+            local minutes = math.floor((newHour % 1) * 60)
+            local message = string.format("You now have ~b~%d~w~ hours and ~b~%d~w~ minutes played.", fullHours, minutes)
+            vRP.EXT.Base.remote._notify(user.source, message)
         end
     end
 end
 
-function hours.tunnel:updateHours(hours)
+function Hours.tunnel:updateHours(hours)
+    if type(hours) ~= "number" or hours <= 0 or hours > 24 then return end
     local user = vRP.users_by_source[source]
     if user then
         vRP:triggerEvent("updateHours", hours)
     end
 end
 
-function hours.event:characterLoad(user)
-  if user.cdata then
-    if user.cdata.hours == nil then
-      user.cdata.hours = 0
+function Hours.event:characterLoad(user)
+    if user.cdata and user.cdata.hours == nil then
+        user.cdata.hours = 0
     end
-  end
 end
 
-
-vRP:registerExtension(hours)
+vRP:registerExtension(Hours)
